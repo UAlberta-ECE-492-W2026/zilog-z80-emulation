3574f11e1621f8ec7f20ac3761417fe7e4c50e18
Cole Haraga <90408054+haragaCole@users.noreply.github.com>
2026-02-10 22:50:24 -0700

    datapath top level file initialization

---
+++ src/hdl/datapath.sv
@@ -0,0 +1,16 @@
+
+`timescale 1ns/1ps
+`include "alu_op.sv"
+
+module datapath (
+    input logic clk,
+    input logic reset,
+
+    
+
+
+)
+endmodule
+// ###########################
+// End of module datapath
+// ###########################

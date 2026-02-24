c531c2a124950ed34a0145311fde2b60c9d920c3
waridh <waridh.git@pm.me>
2026-02-23 17:18:07 -0700

    moved the system into a more minimum viable fsm

--- src/hdl/controller.sv
+++ src/hdl/controller.sv
@@ -2,10 +2,11 @@
 
 /** Module for the controller of the datapath controller architecture.
  */
-module controller (
-   input wire [3:0] detected_opcode,
-   input wire       resetn, /* active low reset */
-   input wire       clk
+module controller (output wire [3:0] aluop,
+
+                   input wire [3:0]  uop,
+                   input wire        reset, /* active high reset */
+                   input wire        clk
    );
    /* parameters */
 
@@ -17,17 +18,37 @@ module controller (
 
    /* instantiation */
    controller_states current_state;
+   controller_states next_state;
+   reg [3:0] aluop_reg;
+
+   /* dataflow */
+
+   assign aluop = aluop_reg;
+
 
    /* behavioural */
 
    /* next state logic */
 
+   always_comb begin: next_state_block
+      next_state = RESET;
+      if (reset) next_state = RESET;
+   end;
+
    /* output logic */
 
+   always_comb begin: output_block
+      aluop_reg = 0;
+      if (reset) aluop_reg = 0;
+   end;
+
    /* state driver */
-   always_ff @(posedge clk, resetn) begin
-      if (!resetn) current_state <= RESET;
+   always_ff @(posedge clk, reset) begin: flip_flop_driver_block
+      if (reset) current_state <= RESET;
       else begin
+         /* we are not doing a reset */
+         current_state <= next_state;
+
       end;
    end;
    endmodule

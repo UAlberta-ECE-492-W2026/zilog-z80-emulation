1f87371fdf5fc9053f99bebfbf67eacbef0b9a8b
waridh <waridh.git@pm.me>
2026-03-14 14:07:46 -0600

    removed mop comment from uop

--- src/enum/uop.sv
+++ src/enum/uop.sv
@@ -4,14 +4,8 @@
  enum for the uop (micro-operation). These represents the states that the
  controller may take, which in turn defines the output of the controller.
 
- One major difference is that special characters like () and spaces can't be used in code
- in place of spaces '_' has been used. in place of () register names are prefixed with 'm'
- the secondary registers (e.g. AF') are denoted with the 'p' suffix
-
- Some similar instructions have been combined into one uop. For example LD r (HL) and LD r (IX+d)
- are both denoted as LD_R_mRd, where R denotes a generic register, not just one of the general
- purpose registers. This results in some isntructions like LD_R_R repeating 'R', despite reffering
-to two different registers.
+ This enum is primarily used by the controller next state logic and the output
+ logic.
 */
 typedef enum [7:0] {
 // misc

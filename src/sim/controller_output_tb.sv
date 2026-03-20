5b7efdf7f261d1856b9dd4957351a24cb27e5c7c
Bach Wongwandanee <bwongwan@Bachs-MacBook-Air.local>
2026-03-20 11:24:05 -0600

    updated the tb to use events

--- src/sim/controller_output_tb.sv
+++ src/sim/controller_output_tb.sv
@@ -3,21 +3,28 @@
 
 /* verilator lint_on UNUSEDSignal */
 module controller_output_tb();
+
+    /* synchronization primitive */
+    event transaction_applied;
+    event test_data_ready;
+    event test_finished;
+
+    /* data declaration */
     string curr_test;
 
-   /* verilator lint_off UNUSEDSignal */
+    /* verilator lint_off UNUSEDSignal */
     /**
      Function that is responsible for displaying the result of a test.
      The pass_test reg is 1 when the test passed, and 0 when the test
      failed.
      */
-    function void display_input_output_expected(input string test_name,
+    task display_input_output_expected(input string test_name,
                                                     uop::uop_t current_state,
                                                 );
         $display("test name: %s", test_name);
         $display("    time          : %0t", $time);
         $display("    current_state : %s", current_state.name);
-    endfunction // display_input_output_expected
+    endtask; // display_input_output_expected
 
 
     typedef struct {
@@ -31,7 +38,6 @@ module controller_output_tb();
     endfunction; // cons_test
 
     task apply_transaction(input test_vector tv);
-            #10;
             dut_intf.current_state = tv.curr_state;
             curr_test = tv.test_name;
             #1;
@@ -48,20 +54,40 @@ module controller_output_tb();
 
     initial begin: test_definition
         testvectors.push_back(cons_test("nop", uop::nop));
+        testvectors.push_back(cons_test("fetch", uop::fetch));
+        testvectors.push_back(cons_test("pc_m2", uop::pc_m2));
+        testvectors.push_back(cons_test("pc_m1", uop::pc_m1));
+        testvectors.push_back(cons_test("sp_m1", uop::sp_m1));
+        testvectors.push_back(cons_test("sp_p2", uop::sp_p2));
+        testvectors.push_back(cons_test("pc_next", uop::pc_next));
+        testvectors.push_back(cons_test("ld_reg_a_reg_b", uop::ld_reg_a_reg_b));
+
+        ->test_data_ready;
     end;
 
-    initial begin
+    initial begin: test_applications
+        wait (test_data_ready.triggered);
         foreach (testvectors[i]) begin
+            #10;
             apply_transaction(testvectors[i]);
+            #1
+            -> transaction_applied;
         end
-        #10 $finish;
+        -> test_finished;
+        $finish;
     end // initial begin
 
-    always begin
-        #11 display_input_output_expected(curr_test,
-                                          dut_intf.current_state
-                                          );
-    end
+    initial begin: test_ender
+        wait (test_finished.triggered);
+        $finish;
+    end;
+
+    always begin: test_printout_block
+        @transaction_applied;
+        display_input_output_expected(curr_test,
+                                      dut_intf.current_state
+                                      );
+    end;
 
     c_to_dp_intf dut_intf();
 

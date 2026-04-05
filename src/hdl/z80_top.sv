f1d330d816c6553a976f9b85f6e749a1040df678
Nikolai <nikolai@insidedesk.com>
2026-04-04 22:29:02 -0600

    more debug stuff

--- src/hdl/z80_top.sv
+++ src/hdl/z80_top.sv
@@ -27,13 +27,15 @@ module z80_top #(
 
     // AXI interface missing
 ); 
+    /* verilator lint_off UNUSEDSIGNAL */
+    // temp since the VGA uses the same pmod as the seven segment display
     logic hsync;            //! horizontal sync (active LOW)
     logic vsync;            //! vertical sync (active LOW)
     logic [3:0] red;        //! red channel (4-bit)
     logic [3:0] green;      //! green channel (4-bit)
     logic [3:0] blue;        //! blue channel (4-bit)
     
-    /* verilator lint_off UNUSEDSIGNAL */
+    
     logic [7:0] main_reg_set [0:7];
     logic [15:0] special_reg_set [0:4];
     /* verilator lint_on UNUSEDSIGNAL */
@@ -49,6 +51,7 @@ module z80_top #(
     assign intf.clk = slow_clk;
     assign intf.reset =  buttons[0];
     assign led6_r = intf.clk;
+    assign led6_g = intf.reset;
     
     reg [31:0]div_count;
     always_ff @(posedge clk) begin
@@ -69,15 +72,23 @@ module z80_top #(
         if (switches == 4'b0000) begin
             byte_to_display = intf.current_state[7:0];
         end else if (switches == 4'b0001) begin
+            byte_to_display = intf.mop_out[7:0];
+        end else if (switches == 4'b0010) begin
+            byte_to_display = memory_mapped_display_byte;
+        end else if (switches == 4'b0011) begin
+            byte_to_display = intf.memory_in;
+        end else if (switches == 4'b0100) begin // PC
             if (buttons[2] == 0) begin
                 byte_to_display = special_reg_set[4][7:0];
             end else begin
                 byte_to_display = special_reg_set[4][15:8];
             end
-        end else if (switches == 4'b0010) begin
-            byte_to_display = intf.mop_out[7:0];
-        end else if (switches == 4'b0011) begin
-            byte_to_display = memory_mapped_display_byte;
+        end else if (switches == 4'b0101) begin // SP
+            if (buttons[2] == 0) begin
+                byte_to_display = special_reg_set[3][7:0];
+            end else begin
+                byte_to_display = special_reg_set[3][15:8];
+            end
         end else begin
             byte_to_display = 8'b10101010;
         end
